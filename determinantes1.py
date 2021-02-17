474a4207926ce1c5c2d583c0bdcb06bc2bb4bb21
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-02-17 12:52:32 -0300

    Displaying results of Ex1 as a table

--- determinantes1.py
+++ determinantes1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def calculateA(size, A=np.zeros(0)):
@@ -27,58 +28,75 @@ def calculateDiagonalA(size, A=np.zeros(0)):
     return A
 
 
-def printDeterminants(A, n, deltas=[0, 1e-3, 1e-2, 1e-1], label=''):
-    print(label, f"(n = {n})" + ': ')
-    # print(A)
+def calculateDeterminants(A, n, deltas=[0, 1e-3, 1e-2, 1e-1], label=''):
+    dets = []
     AtA = np.matmul(np.transpose(A), A)
     I = np.identity(n ** 2, int)
     for delta in deltas:
-        print(f"d ={delta: .1E}: {np.linalg.det(AtA + delta * I)}")
-    print('')
+        dets.append(f"{np.linalg.det(AtA + delta * I)}")
+    return dets
 
 
 def main(path='EP1_dados/'):
-    print('p1:\n')
+
+    deltas = [0, 1e-3, 1e-2, 1e-1]
+
+    # print('\n p1:\n_____\n')
 
     im1_p1 = np.load(path + 'im1/p1.npy')
     im1_n1 = int(im1_p1.size / 2)
-    print('im1_p1:', im1_p1, f"im1_n1 = {im1_n1}\n")
+    # print('im1_p1:', im1_p1, f"im1_n1 = {im1_n1}\n")
     im1_A1 = calculateA(im1_n1)
 
     im2_p1 = np.load(path + 'im2/p1.npy')
     im2_n1 = int(im2_p1.size / 2)
-    print('im2_p1:', im2_p1, f"im2_n1 = {im2_n1}\n")
+    # print('im2_p1:', im2_p1, f"im2_n1 = {im2_n1}\n")
     im2_A1 = calculateA(im2_n1)
 
     im3_p1 = np.load(path + 'im3/p1.npy')
     im3_n1 = int(im3_p1.size / 2)
-    print('im3_p1:', im3_p1, f"im3_n1 = {im3_n1}\n")
+    # print('im3_p1:', im3_p1, f"im3_n1 = {im3_n1}\n")
     im3_A1 = calculateA(im3_n1)
 
-    printDeterminants(im1_A1, im1_n1, label='im1_p1')
-    printDeterminants(im2_A1, im2_n1, label='im2_p1')
-    printDeterminants(im3_A1, im3_n1, label='im3_p1')
+    dets1_1 = calculateDeterminants(im1_A1, im1_n1, label='im1_p1')
+    dets1_2 = calculateDeterminants(im2_A1, im2_n1, label='im2_p1')
+    dets1_3 = calculateDeterminants(im3_A1, im3_n1, label='im3_p1')
 
-    print('p2:\n')
+    # print('\n p2:\n_____\n')
 
     im1_p2 = np.load(path + 'im1/p2.npy')
     im1_n2 = int((im1_p2.size + 2) / 6)
-    print('im1_p2:', im1_p2, f"im1_n2 = {im1_n2}\n")
+    # print('im1_p2:', im1_p2, f"im1_n2 = {im1_n2}\n")
     im1_A2 = calculateDiagonalA(im1_n2)
 
     im2_p2 = np.load(path + 'im2/p2.npy')
-    im2_n2 = int((im2_p2.size + 2)/ 6)
-    print('im2_p2:', im2_p1, f"im2_n2 = {im2_n2}\n")
+    im2_n2 = int((im2_p2.size + 2) / 6)
+    # print('im2_p2:', im2_p1, f"im2_n2 = {im2_n2}\n")
     im2_A2 = calculateDiagonalA(im2_n1)
 
     im3_p2 = np.load(path + 'im3/p2.npy')
-    im3_n2 = int((im3_p2.size + 2)/ 6)
-    print('im3_p2:', im3_p1, f"im3_n2 = {im3_n2}\n")
+    im3_n2 = int((im3_p2.size + 2) / 6)
+    # print('im3_p2:', im3_p1, f"im3_n2 = {im3_n2}\n")
     im3_A2 = calculateDiagonalA(im3_n1)
 
-    printDeterminants(im1_A2, im1_n2, label='im1_p2')
-    printDeterminants(im2_A2, im2_n2, label='im2_p2')
-    printDeterminants(im3_A2, im3_n2, label='im3_p2')
+    dets1_1 = calculateDeterminants(im1_A2, im1_n2, label='im1_p2')
+    dets1_2 = calculateDeterminants(im2_A2, im2_n2, label='im2_p2')
+    dets1_3 = calculateDeterminants(im3_A2, im3_n2, label='im3_p2')
+
+    fig, ax = plt.subplots()
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    ax.axis('tight')
+
+    table = ax.table(colLabels=list(map(lambda delta: f"delta = {delta:.1E}", deltas)), rowLabels=[
+                     ' im1 ', ' im2 ',  ' im3 '], cellText=[dets1_1, dets1_2, dets1_3], loc='center')
+
+    table.auto_set_font_size(False)
+    table.set_fontsize(6)
+    # table.scale(2, 2)
+    fig.tight_layout()
+
+    plt.show()
 
 
 main()
