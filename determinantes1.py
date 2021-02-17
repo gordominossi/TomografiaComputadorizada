9b8ca68601b54c1ee8f5ed0acea5d5071d848e35
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-02-17 12:04:43 -0300

    Adding matrix A for diagonal projections

--- determinantes1.py
+++ determinantes1.py
@@ -1,18 +1,29 @@
 import numpy as np
 
 
-def getA(size):
-    # A1 = np.zeros((6 * n1 - 2, n1 ** 2), int)
-    A = np.zeros((2 * size, size ** 2), int)
+def calculateA(size, A=np.zeros(0)):
+    if A.size == 0:
+        A = np.zeros((2 * size, size ** 2), int)
     for j in range(0, size ** 2):
         for i in range(0, size):
             A[i, j] = int(i == j % size)
         for i in range(0, size):
             A[i + size, j] = int(i == j // size)
-        # for i in range(0, 2 * n1 - 1):
-        #     A[i + 2 * n1, j] = int(i == n1 - 1 + j // n1 - j % n1)
-        # for i in range(0, 2 * n1 - 1):
-        #     A[i + 4 * n1 - 1, j] = int(i == j // n1 + j % n1)
+    return A
+
+
+def calculateDiagonalA(size, A=np.zeros(0)):
+    if A.size == 0:
+        A = np.zeros((6 * size - 2, size ** 2), int)
+    for j in range(0, size ** 2):
+        for i in range(0, size):
+            A[i, j] = int(i == j % size)
+        for i in range(0, size):
+            A[i + size, j] = int(i == j // size)
+        for i in range(0, 2 * size - 1):
+            A[i + 2 * size, j] = int(i == size - 1 + j // size - j % size)
+        for i in range(0, 2 * size - 1):
+            A[i + 4 * size - 1, j] = int(i == j // size + j % size)
     return A
 
 
@@ -27,24 +38,47 @@ def printDeterminants(A, n, deltas=[0, 1e-3, 1e-2, 1e-1], label=''):
 
 
 def main(path='EP1_dados/'):
-    p1 = np.load(path + 'im1/p1.npy')
-    n1 = int(p1.size / 2)
-    print('p1:', p1, f"n1 = {n1}", '\n')
-    A1 = getA(n1)
+    print('p1:\n')
 
-    p2 = np.load(path + 'im2/p1.npy')
-    n2 = int(p2.size / 2)
-    print('p2:', p2, f"n2 = {n2}", '\n')
-    A2 = getA(n2)
+    im1_p1 = np.load(path + 'im1/p1.npy')
+    im1_n1 = int(im1_p1.size / 2)
+    print('im1_p1:', im1_p1, f"im1_n1 = {im1_n1}\n")
+    im1_A1 = calculateA(im1_n1)
 
-    p3 = np.load(path + 'im3/p1.npy')
-    n3 = int(p3.size / 2)
-    print('p3:', p3, f"n3 = {n3}", '\n')
-    A3 = getA(n3)
+    im2_p1 = np.load(path + 'im2/p1.npy')
+    im2_n1 = int(im2_p1.size / 2)
+    print('im2_p1:', im2_p1, f"im2_n1 = {im2_n1}\n")
+    im2_A1 = calculateA(im2_n1)
 
-    printDeterminants(A1, n1, label='im1')
-    printDeterminants(A2, n2, label='im2')
-    printDeterminants(A3, n3, label='im3')
+    im3_p1 = np.load(path + 'im3/p1.npy')
+    im3_n1 = int(im3_p1.size / 2)
+    print('im3_p1:', im3_p1, f"im3_n1 = {im3_n1}\n")
+    im3_A1 = calculateA(im3_n1)
+
+    printDeterminants(im1_A1, im1_n1, label='im1_p1')
+    printDeterminants(im2_A1, im2_n1, label='im2_p1')
+    printDeterminants(im3_A1, im3_n1, label='im3_p1')
+
+    print('p2:\n')
+
+    im1_p2 = np.load(path + 'im1/p2.npy')
+    im1_n2 = int((im1_p2.size + 2) / 6)
+    print('im1_p2:', im1_p2, f"im1_n2 = {im1_n2}\n")
+    im1_A2 = calculateDiagonalA(im1_n2)
+
+    im2_p2 = np.load(path + 'im2/p2.npy')
+    im2_n2 = int((im2_p2.size + 2)/ 6)
+    print('im2_p2:', im2_p1, f"im2_n2 = {im2_n2}\n")
+    im2_A2 = calculateDiagonalA(im2_n1)
+
+    im3_p2 = np.load(path + 'im3/p2.npy')
+    im3_n2 = int((im3_p2.size + 2)/ 6)
+    print('im3_p2:', im3_p1, f"im3_n2 = {im3_n2}\n")
+    im3_A2 = calculateDiagonalA(im3_n1)
+
+    printDeterminants(im1_A2, im1_n2, label='im1_p2')
+    printDeterminants(im2_A2, im2_n2, label='im2_p2')
+    printDeterminants(im3_A2, im3_n2, label='im3_p2')
 
 
 main()
