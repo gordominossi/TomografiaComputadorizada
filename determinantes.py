676e4047f6fd3d59c5f82fc0dc935d04ec6c0aa0
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-02-17 21:13:53 -0300

    Refactoring: Removing duplicate code. Grouping different determinants in an array

--- determinantes.py
+++ determinantes.py
@@ -64,41 +64,33 @@ def main(path='EP1_dados/', deltas=[0, 1e-3, 1e-2, 1e-1]):
     im1_p1 = np.load(path + 'im1/p1.npy')
     im2_p1 = np.load(path + 'im2/p1.npy')
     im3_p1 = np.load(path + 'im3/p1.npy')
+    ims_p1 = [im1_p1, im2_p1, im3_p1]
 
-    im1_n1 = int(im1_p1.size / 2)
-    im1_A1 = calculateA(im1_n1)
-
-    im2_n1 = int(im2_p1.size / 2)
-    im2_A1 = calculateA(im2_n1)
-
-    im3_n1 = int(im3_p1.size / 2)
-    im3_A1 = calculateA(im3_n1)
-
-    # print('\n Calculando determinantes de p1:\n_____\n')
-
-    dets1_1 = calculateDeterminants(im1_A1, im1_n1, deltas)
-    dets1_2 = calculateDeterminants(im2_A1, im2_n1, deltas)
-    dets1_3 = calculateDeterminants(im3_A1, im3_n1, deltas)
+    ims_n1 = []
+    ims_A1 = []
+    dets1 = []
+    print('\n Calculando determinantes de p1...\n')
+    for i in range(len(ims_p1)):
+        ims_n1.append(int(ims_p1[i].size / 2))
+        ims_A1.append(calculateA(ims_n1[i]))
+        dets1.append(calculateDeterminants(
+            ims_A1[i], ims_n1[i], deltas))
 
     # Carrega informações de p2
     im1_p2 = np.load(path + 'im1/p2.npy')
     im2_p2 = np.load(path + 'im2/p2.npy')
     im3_p2 = np.load(path + 'im3/p2.npy')
+    ims_p2 = [im1_p2, im2_p2, im3_p2]
 
-    im1_n2 = int((im1_p2.size + 2) / 6)
-    im1_A2 = calculateDiagonalA(im1_n2)
-
-    im2_n2 = int((im2_p2.size + 2) / 6)
-    im2_A2 = calculateDiagonalA(im2_n1)
-
-    im3_n2 = int((im3_p2.size + 2) / 6)
-    im3_A2 = calculateDiagonalA(im3_n1)
-
-    # print('\n Calculando determinantes de p2:\n_____\n')
-
-    dets2_1 = calculateDeterminants(im1_A2, im1_n2, deltas)
-    dets2_2 = calculateDeterminants(im2_A2, im2_n2, deltas)
-    dets2_3 = calculateDeterminants(im3_A2, im3_n2, deltas)
+    ims_n2 = []
+    ims_A2 = []
+    dets2 = []
+    print('\n Calculando determinantes de p2...\n')
+    for i in range(len(ims_p2)):
+        ims_n2.append(int((ims_p2[i].size + 2) / 6))
+        ims_A2.append(calculateDiagonalA(ims_n2[i]))
+        dets2.append(calculateDeterminants(
+            ims_A2[i], ims_n2[i], deltas))
 
     # Configura a tabela referente a p1 para exibição
     fig1, ax1 = plt.subplots()
@@ -107,7 +99,7 @@ def main(path='EP1_dados/', deltas=[0, 1e-3, 1e-2, 1e-1]):
     ax1.axis('tight')
 
     table1 = ax1.table(colLabels=list(map(lambda delta: f'delta = {delta:.0e}', deltas)), rowLabels=[
-        ' im1 ', ' im2 ',  ' im3 '], cellText=[dets1_1, dets1_2, dets1_3], loc='top')
+        ' im1 ', ' im2 ',  ' im3 '], cellText=dets1, loc='top')
 
     table1.auto_set_font_size(False)
     table1.set_fontsize(6)
@@ -121,7 +113,7 @@ def main(path='EP1_dados/', deltas=[0, 1e-3, 1e-2, 1e-1]):
     ax2.axis('tight')
 
     table2 = ax2.table(colLabels=list(map(lambda delta: f'delta = {delta:.0e}', deltas)), rowLabels=[
-        ' im1 ', ' im2 ',  ' im3 '], cellText=[dets2_1, dets2_2, dets2_3], loc='top')
+        ' im1 ', ' im2 ',  ' im3 '], cellText=dets2, loc='top')
 
     table2.auto_set_font_size(False)
     table2.set_fontsize(6)
